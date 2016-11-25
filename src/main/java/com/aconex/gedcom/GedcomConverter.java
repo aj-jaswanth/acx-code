650b96d6f7a481ff12e690c6f817abc890237a22
Venkata Jaswanth <aj.jaswanth@gmail.com>
2016-11-25 23:57:10 +0530

    Refactor GedcomConverter to get dependencies injected. Made code unit testable.

--- src/main/java/com/aconex/gedcom/GedcomConverter.java
+++ src/main/java/com/aconex/gedcom/GedcomConverter.java
@@ -2,13 +2,14 @@ package com.aconex.gedcom;
 
 public class GedcomConverter {
     private GedcomParser gedcomParser;
-    private XmlElement gedcomXmlElement = new XmlElement("GEDCOM", null, null);
+    private XmlElement rootXmlElement;
 
-    public GedcomConverter(GedcomParser gedcomParser) {
+    public GedcomConverter(GedcomParser gedcomParser, XmlElement rootXmlElement) {
         this.gedcomParser = gedcomParser;
+        this.rootXmlElement = rootXmlElement;
     }
 
     public String toXml() {
-        return gedcomXmlElement.toString();
+        return rootXmlElement.toString();
     }
 }
--- src/test/java/com/aconex/gedcom/GedcomConverterTest.java
+++ src/test/java/com/aconex/gedcom/GedcomConverterTest.java
@@ -6,21 +6,25 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class GedcomConverterTest {
 
     private GedcomConverter gedcomConverter;
+    private GedcomParser gedcomParser;
+    private XmlElement rootXmlElement;
 
     @Before
     public void setUp() {
-        GedcomParser gedcomParser = mock(GedcomParser.class);
-        this.gedcomConverter = new GedcomConverter(gedcomParser);
+        this.gedcomParser = mock(GedcomParser.class);
+        this.rootXmlElement = mock(XmlElement.class);
+        this.gedcomConverter = new GedcomConverter(gedcomParser, rootXmlElement);
     }
 
     @Test
-    public void shouldGiveEmptyGedcomXmlMarkupIfNoInputIsPresent() {
-        String xml = gedcomConverter.toXml();
+    public void shouldReturnXmlMarkUpOfRootXmlElement() {
+        when(rootXmlElement.toString()).thenReturn("<root></root>");
 
-        assertThat(xml, is("<gedcom></gedcom>"));
+        assertThat(gedcomConverter.toXml(), is("<root></root>"));
     }
 }
