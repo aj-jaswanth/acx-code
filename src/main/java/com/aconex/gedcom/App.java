cb5d0c1e1cd112ab4c67901a3d50404edc6bfffc
Venkata Jaswanth <aj.jaswanth@gmail.com>
2016-11-27 12:58:20 +0530

    Use try block to automatically close file resource

--- src/main/java/com/aconex/gedcom/App.java
+++ src/main/java/com/aconex/gedcom/App.java
@@ -6,17 +6,18 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(args[0]));
-        GedcomParser gedcomParser = new GedcomParser();
-        XmlElement xmlElement = new XmlElement("GEDCOM", null, null);
-        GedcomConverter gedcomConverter = new GedcomConverter(gedcomParser, xmlElement);
+        try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+            GedcomParser gedcomParser = new GedcomParser();
+            XmlElement xmlElement = new XmlElement("GEDCOM", null, null);
+            GedcomConverter gedcomConverter = new GedcomConverter(gedcomParser, xmlElement);
 
-        String input;
-        while ((input = br.readLine()) != null) {
-            if (!input.isEmpty()) {
-                gedcomConverter.process(input);
+            String input;
+            while ((input = br.readLine()) != null) {
+                if (!input.isEmpty()) {
+                    gedcomConverter.process(input);
+                }
             }
+            System.out.println(gedcomConverter.toXml());
         }
-        System.out.println(gedcomConverter.toXml());
     }
 }
