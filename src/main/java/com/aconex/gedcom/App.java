6251263da28c5bdb9e52176a4984a9ef0bafa0d9
Venkata Jaswanth <aj.jaswanth@gmail.com>
2016-11-27 12:35:33 +0530

    Read input from file

--- src/main/java/com/aconex/gedcom/App.java
+++ src/main/java/com/aconex/gedcom/App.java
@@ -1,12 +1,12 @@
 package com.aconex.gedcom;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new FileReader(args[0]));
         GedcomParser gedcomParser = new GedcomParser();
         XmlElement xmlElement = new XmlElement("GEDCOM", null, null);
         GedcomConverter gedcomConverter = new GedcomConverter(gedcomParser, xmlElement);
